abb43b17e37d4b09b9145ff32b081cca5801ba09
rakibaakter <rakibaakter635@gmail.com>
2023-08-15 19:38:15 +0600

    reset option added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let count  = 0;
 const increase = document.getElementById('increase-btn');
 const decrease = document.getElementById('decrease-btn');
+const reset = document.getElementById('reset-btn');
 const countValue = document.getElementById('count-value');
 increase.addEventListener("click", function(){
     count += 1;
@@ -8,5 +9,9 @@ increase.addEventListener("click", function(){
 })
 decrease.addEventListener("click", function(){
     count -= 1;
+    countValue.innerText = count;
+})
+reset.addEventListener("click", function(){
+    count = 0;
     countValue.innerText = count;
 })
